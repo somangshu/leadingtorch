1d4280e3afe625df36d1f3b893bad910f27a130c
somangshu <somangshu@github.com>
2020-01-19 10:58:16 +0530

    url helpers changed to regular html style

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,12 +9,12 @@
     <link rel="shortcut icon" href="/images/favicon.ico" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/style.css">
-    <link href="{{ url('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     <title>Leading Torch LLC</title>
 </head>
 <body>
     <div id="root"></div>
     <div id="snacker"></div>
-    <script src="{{ url('js/app.js') }}"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
